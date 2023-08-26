9c61bed290bbd821db6f3cbd6e849fda7f742e36
Dev Chauhan <devccode8@gmail.com>
2023-08-26 12:17:10 +0530

    v1.5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import {logo} from "../assets/"
 const Hero = () => {
   return (
     <header className='w-[600px] flex justify-center items-center flex-col items-start'>
-        <h1 className='head_text'>Simplify Complex Articles with <br className='max-md:hidden' /><span className='orange_gradient'>Intelligent Summarization</span></h1>
+        <h1 className='text-4xl font-bold text-center'>Simplify Complex Articles with <br className='max-md:hidden' /><span className='orange_gradient'>Intelligent Summarization</span></h1>
         <h2 className='desc'>Simplify your reading experience with Summify, an advanced article summarizer adept at effortlessly transforming lengthy articles into concise and lucid summaries.</h2>
     </header>
   )
